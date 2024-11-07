15cd562261626794b2d1d72af26630a8a4fbde15
Luiz Fernando <fernandosena1@icloud.com>
2024-11-07 08:10:43 -0300

    initial commit

---
+++ src/test/java/com/apis/consultasApi/ConsultasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apis.consultasApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultasApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
